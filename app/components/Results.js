d43e0e360714776fa2ff81cf077107c8d6ccdf4b
Vasyl Shpak <vasyl.shpak4@gmail.com>
2020-08-23 19:17:11 +0200

    feat: add loading component
    Create and add logic for the loading component:
    - default Props,
    - styles inside Loading component

--- app/components/Results.js
+++ app/components/Results.js
@@ -10,6 +10,7 @@ import {
 } from "react-icons/fa";
 import Card from "./Card";
 import PropTypes from "prop-types";
+import Loading from "./Loading";
 
 function ProfileList({ profile }) {
   return (
@@ -80,7 +81,7 @@ export default class Results extends React.Component {
     const { winner, loser, error, loading } = this.state;
 
     if (loading === true) {
-      return <p>LOADING</p>;
+      return <Loading />;
     }
 
     if (error) {
@@ -120,4 +121,9 @@ Results.propTypes = {
   playerOne: PropTypes.string.isRequired,
   playerTwo: PropTypes.string.isRequired,
   onReset: PropTypes.func.isRequired,
+};
+
+Loading.defaultProps = {
+  text: "Loading",
+  speed: 300,
 };
