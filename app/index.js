052805a7bbb2da25240d225eddb15c881afa0272
Vasyl Shpak <vasyl.shpak4@gmail.com>
2020-08-25 22:38:27 +0200

    add: theme provider

--- app/index.js
+++ app/index.js
@@ -3,13 +3,28 @@ import ReactDom from "react-dom";
 import "./index.css";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
+import { ThemeProvider } from "./contexts/theme";
 
 class App extends React.Component {
+  constructor() {
+    super(props);
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: thee === "light" ? "dark" : "light",
+        }));
+      },
+    };
+  }
   render() {
     return (
-      <div className="component">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="component">
+          <Battle />
+        </div>
+      </ThemeProvider>
     );
   }
 }
