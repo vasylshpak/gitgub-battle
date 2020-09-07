bdb07a09e7b55805e434f95451ffaf59738e30d5
Vasyl Shpak <vasyl.shpak4@gmail.com>
2020-09-07 21:42:43 +0200

    update index.js

--- app/index.js
+++ app/index.js
@@ -26,7 +26,7 @@ class App extends React.Component {
           <div className="container">
             <Nav />
 
-            <Popular />
+            <Battle />
           </div>
         </div>
       </ThemeProvider>
