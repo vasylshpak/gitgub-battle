acea44a6f338609ca5e1aa69ab542eb954e36432
Vasyl Shpak <vasyl.shpak4@gmail.com>
2020-09-09 22:08:03 +0200

    add: Routing
    Navigate from "Popular" to "Battle" component

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,39 @@
 import React from "react";
 import { ThemeConsumer } from "../contexts/theme";
+import { NavLink } from "react-router-dom";
+
+const activeStyle = {
+  color: "rgb(100, 200, 31)",
+};
 
 export default function Nav() {
   return (
     <ThemeConsumer>
       {({ theme, toggleTheme }) => (
         <nav className="row space-between">
+          <ul className="row nav">
+            <li>
+              <NavLink
+                to="/"
+                exact
+                activeStyle={activeStyle}
+                className="nav-link"
+                rel="stylesheet"
+              >
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/battle"
+                activeStyle={activeStyle}
+                className="nav-link"
+                rel="stylesheet"
+              >
+                Battle
+              </NavLink>
+            </li>
+          </ul>
           <button
             style={{ fontSize: 30 }}
             className="btn-clear"
