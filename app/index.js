11103c9f39e59d6cf8000bcecc40af7e77eb5591
Vasyl Shpak <vasyl.shpak4@gmail.com>
2020-09-15 22:53:49 +0200

    add queryString

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ import Battle from "./components/Battle";
 import { ThemeProvider } from "./contexts/theme";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Results from "./components/Results";
 
 class App extends React.Component {
   constructor(props) {
@@ -28,7 +29,8 @@ class App extends React.Component {
             <div className="container">
               <Nav />
               <Route exact path="/" component={Popular} />
-              <Route path="/battle" component={Battle} />
+              <Route exact path="/battle" component={Battle} />
+              <Route path="/battle/results" component={Results} />
             </div>
           </div>
         </ThemeProvider>
